03b24fa38b2712245c6486955af801e78a398cbc
Kamran Ali <kamran.ali37@hotmail.com>
2021-04-11 17:36:20 +0200

    updated docs

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
+# The Mirror Project
+**Detection of Implicit Racial Bias using EEG Data**
+
+- [The Mirror](#the-mirror-project)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installing](#installing)
+    - [Running Locally](#running-locally)
+  - [Built With](#built-with)
+  - [Authors](#authors)
+  - [License](#license)
+  - [Acknowledgments](#acknowledgments)
 
 
-Command to run (For MAC):
-python openbci.py --board-id 2 --serial-port /dev/cu.usbserial-DM02587F
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+### Prerequisites
+
+* [Python](https://www.python.org/)
+  
+
+### Installing
+
+Clone or download the repository to your local machine.
+Then run `conda create --name the-mirror python=3.8` to create a conda environment called "the-mirror" with the correct python version. The run `conda activate the-mirror`. Since the requirements.txt is pip-syntaxed, run `pip install -r requirements.txt` after.
+
+### Running Locally
+
+Local app only records EEG data using OpenBCI hardware. For the test we used [Psytoolkit Online IAT Racism](https://www.psytoolkit.org/c/3.3.2/survey?s=DfJfv) online cognitive-psychological experiment. Psytoolkit can also be used offline by just opening `iat_race.html` file in any modern browser. 
+
+*IMPORTANT: at the end of the experiment closing the running python code is important to save the final data from OpenBCI board. Pressing "q" will quit the program and save the data as well in the same directory. Closing the code in terminal will also save the data into the file*
+
+*To run local app:*
+
+**On MAC:**
+* python main.py --board-id 2 --serial-port /dev/cu.usbserial-DM02587F
+
+**On Windows:**
+* python main.py --board-id 2 --serial-port COM3
+
+**NOTE: Please verify the port names in your local PC**
+
+
+## Built With
+
+* [Python](https://python.org/) - Python Web Framework
+* [Psytoolkit](www.psytoolkit.org/) - Psytoolkit for running cognitive-psychological experiments and surveys
+* [OpenBCI](https://shop.openbci.com/collections/frontpage/products/cyton-daisy-biosensing-boards-16-channel?variant=38959256526) OpenBCI cyton and daisy boards
+
+
+## Authors
+
+* **Siddhant Gadamsetti** - *Developer* - [MSc Digital Health, Hasso Plattner Institute Potsdam, Germany](Siddhant.Gadamsetti@student.hpi.uni-potsdam.de)
+* **Florian Hermes** - *Developer* - [MSc Digital Health, Hasso Plattner Institute Potsdam, Germany](Florian.Hermes@student.hpi.uni-potsdam.de)
+* **Kamran Ali** - *Developer* - [MSc Digital Health, Hasso Plattner Institute Potsdam, Germany](Kamran.Ali@student.hpi.uni-potsdam.de)
+
+## License
+
+* *No Copyright from us on code contained in main.py*
+* *For Copyrights of libraries and tools used in the app please visit respective library documents, library list is included in requirements.txt*
+* *For psytoolkit.org please visit [Their Website](www.psytoolkit.org)*
+
--- main.py
+++ main.py
@@ -5,31 +5,67 @@ import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 from pynput.keyboard import Listener, Key
-import  matplotlib
-matplotlib.use ('Agg')
+import matplotlib
+import atexit
+
+matplotlib.use("Agg")
 from datetime import datetime
 
 key_press = []
 
+
 def main():
+    """Main method to start the app and responsible to receive and process args"""
     BoardShim.enable_dev_board_logger()
 
     parser = argparse.ArgumentParser()
     # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
-                        default=0)
-    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
-    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
-                        default=0)
-    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
-    parser.add_argument('--serial-port', type=str, help='serial port', required=True, default='')
-    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
-    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
-    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
-    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
-    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
-                        required=True)
-    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        help="timeout for device discovery or connection",
+        required=False,
+        default=0,
+    )
+    parser.add_argument(
+        "--ip-port", type=int, help="ip port", required=False, default=0
+    )
+    parser.add_argument(
+        "--ip-protocol",
+        type=int,
+        help="ip protocol, check IpProtocolType enum",
+        required=False,
+        default=0,
+    )
+    parser.add_argument(
+        "--ip-address", type=str, help="ip address", required=False, default=""
+    )
+    parser.add_argument(
+        "--serial-port", type=str, help="serial port", required=True, default=""
+    )
+    parser.add_argument(
+        "--mac-address", type=str, help="mac address", required=False, default=""
+    )
+    parser.add_argument(
+        "--other-info", type=str, help="other info", required=False, default=""
+    )
+    parser.add_argument(
+        "--streamer-params",
+        type=str,
+        help="streamer params",
+        required=False,
+        default="",
+    )
+    parser.add_argument(
+        "--serial-number", type=str, help="serial number", required=False, default=""
+    )
+    parser.add_argument(
+        "--board-id",
+        type=int,
+        help="board id, check docs to get a list of supported boards",
+        required=True,
+    )
+    parser.add_argument("--file", type=str, help="file", required=False, default="")
     args = parser.parse_args()
 
     params = BrainFlowInputParams()
@@ -50,53 +86,87 @@ def main():
 
     start_test()
 
+
 def start_test():
+    """Starts the test session setting up the lister for key inputs"""
     with Listener(on_press=start_in) as listener:  # Setup the listener
         listener.join()  # Join the thread to the main thread
     print("\nStarting test.")
     start_stream()
 
+
 def start_stream():
+    """Prepares and starts the data stream from the OpenBCI Board"""
     print("\nStarting data acquisition.")
     board.prepare_session()
     board.start_stream(45000, input)
     key_marker()
 
+
 def key_marker():
+    """Send key press markers to the OpenBCI board which ultimately marks the data and saves it"""
     print("\nWaiting for key input.")
     with Listener(on_press=key_in) as listener:
         listener.join()
     end_test()
 
+
 def end_test():
+    """Ends the test and flushes the saved data in a file in same directory"""
     data = board.get_board_data()
     board.stop_stream()
     board.release_session()
     now = datetime.now()
 
-    file = now.strftime("%d.%m.%Y_%H.%M.%S")+"_eeg_data.csv"
-    DataFilter.write_file(data, file, 'w')
+    file = now.strftime("%d.%m.%Y_%H.%M.%S") + "_eeg_data.csv"
+    DataFilter.write_file(data, file, "w")
     print("\nTest ended.")
 
+
 def start_in(key):
+    """In the start of the experiments double space press is required, this handles the double space
+
+    Args:
+        key (pynput Key): Key Pressed
+
+    Returns:
+        [bool]: False if there are already two space presses.
+    """
     if key == Key.space:
         key_press.append(1)
-    if (len(key_press) > 1):
+    if len(key_press) > 1:
         return False
 
+
 def key_in(key):
+    """Handles different key presses and sends board markers on the basis of different keys
+
+    Args:
+        key (pynput Key): Key Pressed
+
+    Returns:
+        [bool]: False if user presses "q"
+    """
     if key == Key.space:
         board.insert_marker(3)
         print("#####   Block ended   #####")
-    elif key.char == 'e':
+    elif key.char == "e":
         board.insert_marker(1)
         print("e pressed")
-    elif key.char == 'i':
+    elif key.char == "i":
         board.insert_marker(2)
         print("i pressed")
 
-    elif key.char == 'q':
+    elif key.char == "q":
         return False
+
+
+def exit_handler():
+    print("Test Ended")
+    end_test()
+
+
+atexit.register(exit_handler)
 
 if __name__ == "__main__":
     main()
